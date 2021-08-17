fa7f810b968d45a0a50473a5ae691f20b0fcc8ac
kjenson <kenneth.jenson@gmail.com>
2021-08-17 13:29:09 -0600

    remove files

--- comma-1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int amount = 1'999'999'999;
-    cout << amount << endl;
-}
--- swap-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main() {
-    int x = 1, y = 2;
-    swap(x,y);
-    cout << x << ", " << y << endl;
-}
