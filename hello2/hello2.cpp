86a684ddb81162023cfe0a15ce2ff5ed90db8dce
kjenson <kenneth.jenson@gmail.com>
2021-08-17 13:40:31 -0600

    Added hello files

---
+++ hello2/hello2.cpp
@@ -0,0 +1,11 @@
+// hello2.cpp: A second C++ program--says "Hello" to the user
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "\n";
+}
